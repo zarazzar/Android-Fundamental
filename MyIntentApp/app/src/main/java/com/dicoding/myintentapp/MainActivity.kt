0453107528d37a6872534e72766dca83efbcf81d
zaraazzar <azzar.arza18@gmail.com>
2023-08-22 04:08:24 +0700

    added moveObject

--- MyIntentApp/app/src/main/java/com/dicoding/myintentapp/MainActivity.kt
+++ MyIntentApp/app/src/main/java/com/dicoding/myintentapp/MainActivity.kt
@@ -1,14 +1,28 @@
 package com.dicoding.myintentapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-
+import android.widget.TextView
+import androidx.activity.result.contract.ActivityResultContracts
 
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var tvResult: TextView
+    private val resultLauncher = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == MoveForResultActivity.RESULT_CODE && result.data != null) {
+            val selectedValue =
+                result.data?.getIntExtra(MoveForResultActivity.EXTRA_SELECTED_VALUE, 0)
+            tvResult.text = "Hasil : $selectedValue"
+        }
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,25 +33,58 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         val btnMoveWithDataActivity: Button = findViewById(R.id.btn_moveWithDataActivity)
         btnMoveWithDataActivity.setOnClickListener(this)
+
+        val btnMoveWithObject: Button = findViewById(R.id.btn_moveActivityObject)
+        btnMoveWithObject.setOnClickListener(this)
+
+        val btnDialPhone: Button = findViewById(R.id.btn_dialNumber)
+        btnDialPhone.setOnClickListener(this)
+
+        val btnMoveForResult: Button = findViewById(R.id.btn_move_for_result)
+        btnMoveForResult.setOnClickListener(this)
+
+        tvResult = findViewById(R.id.tv_result)
     }
 
     override fun onClick(v: View?) {
-        when (v?.id){
-            R.id.btn_moveActivity ->{
-                val moveIntent = Intent(this@MainActivity,MoveActivity::class.java)
+        when (v?.id) {
+            R.id.btn_moveActivity -> {
+                val moveIntent = Intent(this@MainActivity, MoveActivity::class.java)
                 startActivity(moveIntent)
             }
+
             R.id.btn_moveWithDataActivity -> {
-                val Intent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
-                Intent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Azzar Rizz")
-                Intent.putExtra(MoveWithDataActivity.EXTRA_AGE,20)
-                startActivity(Intent)
+                val moveDataIntent = Intent(this@MainActivity, MoveWithDataActivity::class.java)
+                moveDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "Azzar Rizz")
+                moveDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 20)
+                startActivity(moveDataIntent)
             }
+
+            R.id.btn_moveActivityObject -> {
+                val person = Person("AzzarRizky", 5, "azzar.arza@gmail", "Palembang")
+
+                val moveObjectIntent = Intent(this@MainActivity, MoveWithObjectActivity::class.java)
+                moveObjectIntent.putExtra(MoveWithObjectActivity.EXTRA_PERSON, person)
+                startActivity(moveObjectIntent)
+            }
+
+            R.id.btn_dialNumber -> {
+                val phoneNumber = "085156620557"
+                val dialPhoneIntent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:$phoneNumber"))
+                startActivity(dialPhoneIntent)
+            }
+
+            R.id.btn_move_for_result -> {
+                val moveForResultIntent =
+                    Intent(this@MainActivity, MoveForResultActivity::class.java)
+                resultLauncher.launch(moveForResultIntent)
+            }
+
 
         }
     }
 
 
-    }
+}
 
 
---
+++ MyIntentApp/app/src/main/java/com/dicoding/myintentapp/Person.kt
@@ -0,0 +1,11 @@
+package com.dicoding.myintentapp
+
+import android.os.Parcel
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Person(val name: String?,
+                  val age: Int?,
+                  val email: String?,
+                  val city: String?  ): Parcelable
