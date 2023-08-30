6d12e23693834d7454304a91cdab9f571d3384fb
zaraazzar <azzar.arza18@gmail.com>
2023-08-30 20:40:17 +0700

    flexible fragment in one activity

--- MyFlexibleFragment/app/src/main/java/com/dicoding/myflexiblefragment/HomeFragment.kt
+++ MyFlexibleFragment/app/src/main/java/com/dicoding/myflexiblefragment/HomeFragment.kt
@@ -25,8 +25,16 @@ class HomeFragment : Fragment(), View.OnClickListener {
         btnCategory.setOnClickListener(this)
     }
 
-    override fun onClick(v: View?) {
-
+    override fun onClick(v: View) {
+        if (v.id == R.id.btn_category){
+            val categoryFragment = CategoryFragment()
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, categoryFragment,CategoryFragment::class.java.simpleName)
+                addToBackStack(null) //if deleted, when we pressed back button it'll close the app
+                commit()
+            }
+        }
     }
 
 
