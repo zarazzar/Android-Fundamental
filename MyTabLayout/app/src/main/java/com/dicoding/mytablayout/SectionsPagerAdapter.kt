7a277a0010315d595d4d6b49ee716b5532d1078c
zaraazzar <azzar.arza18@gmail.com>
2023-09-04 06:49:13 +0700

    TabLayout

---
+++ MyTabLayout/app/src/main/java/com/dicoding/mytablayout/SectionsPagerAdapter.kt
@@ -0,0 +1,30 @@
+package com.dicoding.mytablayout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+
+class SectionsPagerAdapter(activity: AppCompatActivity) : FragmentStateAdapter(activity) {
+
+
+    override fun createFragment(position: Int): Fragment {
+//      var fragment : Fragment? = null
+//        when(position) {
+//            0 -> fragment = HomeFragment()
+//            1 -> fragment = ProfileFragment()
+//        }
+//        return fragment as Fragment
+
+        val fragment = HomeFragment()
+        fragment.arguments = Bundle().apply {
+            putInt(HomeFragment.ARG_SECTION_NUMBER, position + 1)
+        }
+        return fragment
+    }
+
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+}
