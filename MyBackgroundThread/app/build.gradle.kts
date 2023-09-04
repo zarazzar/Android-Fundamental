23e1607f067e2043ffdc462a4c1f672cd265ab27
zaraazzar <azzar.arza18@gmail.com>
2023-09-04 12:49:44 +0700

    couruntines

--- MyBackgroundThread/app/build.gradle.kts
+++ MyBackgroundThread/app/build.gradle.kts
@@ -40,7 +40,9 @@ android {
 }
 
 dependencies {
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.9.0")
--- MyBackgroundThread/app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
+++ MyBackgroundThread/app/src/main/java/com/dicoding/mybackgroundthread/MainActivity.kt
@@ -2,7 +2,16 @@ package com.dicoding.mybackgroundthread
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.lifecycle.lifecycleScope
 import com.dicoding.mybackgroundthread.databinding.ActivityMainBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.util.concurrent.Executor
+import java.util.concurrent.Executors
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,20 +22,48 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //menggunakan executor and handler
+//        val executor = Executors.newSingleThreadExecutor()
+//        val handler = Handler(Looper.getMainLooper())
+
         binding.btnStart.setOnClickListener {
-            try {
-                //simulate process compressing
-                for ( i in 0..10) {
-                    Thread.sleep(500)
+//            executor.execute {
+//                try {
+//                    //simulate process compressing
+//                    for (i in 0..10) {
+//                        Thread.sleep(500)
+//                        val precentage = i * 10
+//
+//                        handler.post {
+//
+//                            if (precentage == 100) {
+//                                binding.tvStatus.setText(R.string.task_completed)
+//                            } else {
+//                                binding.tvStatus.text =
+//                                    String.format(getString(R.string.compressing), precentage)
+//                            }
+//                        }
+//                    }
+
+//                } catch (e: InterruptedException) {
+//                    e.printStackTrace()
+//                }
+
+            lifecycleScope.launch(Dispatchers.Default) {
+                //simulate process in background thread
+                for (i in 0..10){
+                    delay(500)
                     val precentage = i * 10
-                    if (precentage == 100) {
-                        binding.tvStatus.setText(R.string.task_completed)
-                    }else{
-                        binding.tvStatus.text = String.format(getString(R.string.compressing), precentage)
+                    withContext(Dispatchers.Main) {
+                        //update ui in main thread
+                        if (precentage == 100 ){
+                            binding.tvStatus.setText(R.string.task_completed)
+                        } else {
+                            binding.tvStatus.text= String.format(getString(R.string.compressing),precentage )
+                        }
                     }
+
                 }
-            } catch (e: InterruptedException) {
-                e.printStackTrace()
             }
         }
 
