c4da09310fcf12993b47532ff69dee3b95c683db
zaraazzar <azzar.arza18@gmail.com>
2023-09-06 19:35:23 +0700

    ViewModel and LifeCycle

---
+++ MyViewModel/app/src/main/java/com/dicoding/myviewmodel/MainActivity.kt
@@ -0,0 +1,52 @@
+package com.dicoding.myviewmodel
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.myviewmodel.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+//    private lateinit var viewModel: MainViewModel
+    private val  viewModel: MainViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        displayResult()
+
+        binding.btnCalculate.setOnClickListener {
+
+            val width = binding.edtWidth.text.toString()
+            val height = binding.edtHeight.text.toString()
+            val length = binding.edtLength.text.toString()
+
+            when {
+                width.isEmpty() -> {
+                    binding.edtWidth.error = "Masih kosong"
+                }
+                height.isEmpty() -> {
+                    binding.edtHeight.error = "Masih kosong"
+                }
+                length.isEmpty() -> {
+                    binding.edtLength.error = "Masih kosong"
+                }
+                else -> {
+                    viewModel.calculate(width, height, length)
+                    displayResult()
+                }
+            }
+
+
+        }
+
+    }
+
+    private fun displayResult() {
+        binding.tvResult.text = viewModel.result.toString()
+    }
+}
