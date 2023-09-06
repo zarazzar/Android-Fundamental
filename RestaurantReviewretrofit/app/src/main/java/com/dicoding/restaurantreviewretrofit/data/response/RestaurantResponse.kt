8636adaa6d4920308be337a2bb9824ea8cfe6473
zaraazzar <azzar.arza18@gmail.com>
2023-09-06 23:29:13 +0700

    live data with API

--- RestaurantReviewretrofit/app/src/main/java/com/dicoding/restaurantreviewretrofit/data/response/RestaurantResponse.kt
+++ RestaurantReviewretrofit/app/src/main/java/com/dicoding/restaurantreviewretrofit/data/response/RestaurantResponse.kt
@@ -13,3 +13,14 @@ data class RestaurantResponse(
 	@field:SerializedName("message")
 	val message: String
 )
+
+data class PostReviewResponse(
+	@field:SerializedName("customerReviews")
+	val customerReviews: List<CustomerReviewsItem>,
+
+	@field:SerializedName("error")
+	val error: Boolean,
+
+	@field:SerializedName("message")
+	val message: String
+)
--- RestaurantReviewretrofit/app/src/main/java/com/dicoding/restaurantreviewretrofit/data/retrofit/ApiService.kt
+++ RestaurantReviewretrofit/app/src/main/java/com/dicoding/restaurantreviewretrofit/data/retrofit/ApiService.kt
@@ -1,4 +1,22 @@
 package com.dicoding.restaurantreviewretrofit.data.retrofit
 
+import com.dicoding.restaurantreviewretrofit.data.response.PostReviewResponse
+import retrofit2.Call
+import com.dicoding.restaurantreviewretrofit.data.response.RestaurantResponse
+import retrofit2.http.*
+
 interface ApiService {
+    @GET("detail/{id}")
+    fun getRestaurant(
+        @Path("id") id : String
+    ): Call<RestaurantResponse>
+
+    @FormUrlEncoded
+    @Headers("Authorization: token 12345")
+    @POST("review")
+    fun postReview(
+        @Field("id") id: String,
+        @Field("name") name: String,
+        @Field("review") review: String
+    ): Call<PostReviewResponse>
 }
