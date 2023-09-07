31ecf80cb595fe8d120bb68602169e3801d5c7ae
zaraazzar <azzar.arza18@gmail.com>
2023-09-07 18:08:26 +0700

    Init Project

---
+++ FirstGithubUser/app/src/main/java/com/dicoding/firstgithubuser/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.dicoding.firstgithubuser.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.dicoding.firstgithubuser.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
