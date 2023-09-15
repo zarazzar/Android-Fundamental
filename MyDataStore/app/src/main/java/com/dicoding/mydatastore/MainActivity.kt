444c6dbab51ba5d8484cf5fc75eae6b85df9a89a
zaraazzar <azzar.arza18@gmail.com>
2023-09-15 10:48:26 +0700

    DataStore Exercise

---
+++ MyDataStore/app/src/main/java/com/dicoding/mydatastore/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.dicoding.mydatastore
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.CompoundButton
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.lifecycle.ViewModelProvider
+import com.dicoding.mydatastore.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val pref = SettingPreferences.getInstance(application.dataStore)
+
+        val mainVieModel = ViewModelProvider(this, ViewModelFactory(pref)).get(
+            MainViewModel::class.java
+        )
+
+        mainVieModel.getThemeSettings().observe(this) { isDarkModeActive: Boolean ->
+            if (isDarkModeActive) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                binding.switchTheme.isChecked = true
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                binding.switchTheme.isChecked = false
+            }
+
+        }
+
+        binding.switchTheme.setOnCheckedChangeListener { _: CompoundButton, isChecked: Boolean ->
+            mainVieModel.saveThemeSetting(isChecked)
+        }
+    }
+}
